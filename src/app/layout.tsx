110caad9a00d595c75696847de48b7ff4f50a17c
Stian Haga <hagastian@gmail.com>
2025-09-27 09:46:02 +0200

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-[100vh]`}
       >
         <ClerkProvider>
-          <ConvexClientProvider>Hello{children}</ConvexClientProvider>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
         </ClerkProvider>
       </body>
     </html>
